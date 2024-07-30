49c1b2d4add2a3b6e0888008c6b2baf221fb4a8f
Ed <edca81@gmail.com>
2024-07-29 22:31:39 -0600

    Users views
    Users templates, views, layouts and urls settings

--- pdmcrud/crud/urls.py
+++ pdmcrud/crud/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('users', views.users, name='users'),
+    path('users/create', views.users_create, name='users_create'),
+    path('users/edit', views.users_edit, name='users_edit'),
     path('about', views.about, name='about'),
 ]
--- pdmcrud/crud/views.py
+++ pdmcrud/crud/views.py
@@ -5,8 +5,15 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'pages/index.html')
 
+# USERS views
 def users(request):
     return render(request,'users/index.html')
+
+def users_create(request):
+    return render(request,'users/create.html')
+
+def users_edit(request):
+    return render(request,'users/edit.html')
 
 def about(request):
     return render(request,'pages/about.html')
